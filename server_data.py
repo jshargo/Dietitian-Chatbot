8f0acd6e33bf6df29e8f5f2dd8a3beb4362e54a4
jshargo <joshshargo@gmail.com>
2024-08-22 17:23:56 -0500

    fixes

--- server_data.py
+++ server_data.py
@@ -2,13 +2,13 @@ import gdown
 import zipfile
 
 file_id = '15nZSasyaTPjJs5jXLE-wA34T0Ju-tjIO' 
-destination = 'text_chunks_and_embeddings_df.csv'
+download_path = 'text_chunks_and_embeddings_df.csv'
 
 url = f"https://drive.google.com/uc?id={file_id}"
-gdown.download(url, destination, quiet=False)
+gdown.download(url, download_path, quiet=False)
 
-with zipfile.ZipFile(destination, 'r') as zip_ref:
-    zip_ref.extractall('downloads/embeddings')
+with zipfile.ZipFile(download_path, 'r') as ziphandler:
+    ziphandler.extractall('dataset_folder')
 
 
 
